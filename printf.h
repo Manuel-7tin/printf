bb284121a8c753755fdee4989341f94df2d52574
Ebi-Fredrick Emmanuel <ebifredrick07@gmail.com>
2023-05-31 21:15:41 +0100

    Create header file

---
+++ printf.h
@@ -0,0 +1,20 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdarg.h> 
+#include <limits.h>
+#include <stdint.h>
+
+int print_str(char *s);
+char *int_to_str(int n);
+char *unsigned_int_to_str(unsigned int n);
+char *reverse_str(char *s, int j, int num);
+char *int_to_hex(int n, char c);
+char *int_to_oct(unsigned int n);
+char *ptr_to_hex(void* ptr);
+int add(int a, int b);
+#endif /*PRINTF_H*/
